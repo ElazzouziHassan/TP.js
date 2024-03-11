3d8c071fb8b90eb0631ab1a37dc38d169d3c1030
Wizardy <wizardhassan99@gmail.com>
2024-03-11 17:23:25 +0000

    commit

--- main.js
+++ main.js
@@ -8,21 +8,28 @@ body.appendChild(heading)
 heading.style.color = "green"
 heading.style.fontSize = "36px"
 
-let p1 = document.createElement('p');
-let p2 = document.createElement('p');
-let p3 = document.createElement('p');
+for (let i=0; i<3; i++){
+  let p = document.createElement('p');
+  p.textContent = "Paragraphe content";
+  p.style.color = "blue";
+  p.style.fontSize = "24px"
+  body.append(p);
+}
+// let p1 = document.createElement('p');
+// let p2 = document.createElement('p');
+// let p3 = document.createElement('p');
 
-body.append(p1);
-body.append(p2);
-body.append(p3);
+// body.append(p1);
+// body.append(p2);
+// body.append(p3);
 
-document.querySelectorAll('p').forEach(
-  p => {
-    p.textContent = "Paragraphe content";
-    p.style.color = "blue";
-    p.style.fontSize = "24px"
-  }
-);
+// document.querySelectorAll('p').forEach(
+//   p => {
+//     p.textContent = "Paragraphe content";
+//     p.style.color = "blue";
+//     p.style.fontSize = "24px"
+//   }
+// );
 
 let allParas = document.querySelectorAll('p');
 allParas.forEach(
